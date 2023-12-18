e49a86d7dbb0b1cf1a38177669a092d76af77d35
kostamilorava <kosta.milorava@gmail.com>
2023-12-18 21:25:34 +0100

    update zephyr service provider to include publishable resources

--- src/ZephyrServiceProvider.php
+++ src/ZephyrServiceProvider.php
@@ -5,9 +5,16 @@ namespace RedberryProducts\Zephyr;
 use RedberryProducts\Zephyr\Commands\GenerateCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Spatie\MediaLibrary\MediaCollections\Models\Observers\MediaObserver;
 
 class ZephyrServiceProvider extends PackageServiceProvider
 {
+    public function boot()
+    {
+        $this->registerPublishables();
+    }
+
     public function configurePackage(Package $package): void
     {
         /*
@@ -21,5 +28,18 @@ class ZephyrServiceProvider extends PackageServiceProvider
             ->hasViews()
             ->hasMigration('create_laravel-zephyr_table')
             ->hasCommands([GenerateCommand::class]);
+    }
+
+
+    protected function registerPublishables(): void
+    {
+        if (! $this->app->runningInConsole()) {
+            return;
+        }
+
+        $this->publishes([
+            __DIR__.'/../config/zephyr.php' => config_path('zephyr.php'),
+        ], 'config');
+
     }
 }
