61f574761be0a376406175770c5ad102a8def4b5
Kosta <kosta.milorava@gmail.com>
2024-11-22 00:43:53 +0400

    Remove extra imports

--- src/ZephyrServiceProvider.php
+++ src/ZephyrServiceProvider.php
@@ -3,7 +3,6 @@
 namespace RedberryProducts\Zephyr;
 
 use RedberryProducts\Zephyr\Commands\GenerateCommand;
-use RedberryProducts\Zephyr\Facades\ZephyrTestFilesManager;
 use RedberryProducts\Zephyr\Services\ApiService;
 use RedberryProducts\Zephyr\Services\TestFilesManagerService;
 use Spatie\LaravelPackageTools\Package;
